3ef5e046e054e9050863af7d75ac0699da3ab172
gdhanda26 <113314719+gdhanda26@users.noreply.github.com>
2023-12-05 05:02:39 -0500

    Update terraingenerator.h

--- src/terraingenerator.h
+++ src/terraingenerator.h
@@ -4,13 +4,11 @@
 #include <glm/glm.hpp>
 #include <map>
 
-
-
 class TerrainGenerator
 {
 public:
     TerrainGenerator();
-    std::vector<glm::mat4> createTranslationMatricesForChunk(int chunkX, int chunkY);
+    std::map<std::pair<int, int>, std::vector<glm::mat4>> createTranslationMatricesForChunk(int chunkX, int chunkY);
     static float getOffset();
     static const int chunkSize = 10;
     static const int maxChunkHeight = 16;
@@ -18,13 +16,13 @@ public:
     static const int offset = 1;
     glm::vec3 playerPosition;
     int renderDistance = 2; // Number of chunks to render in each direction from the player
-    std::map<std::pair<int, int>, std::vector<glm::mat4>> chunkMatrices;
+    std::map<std::pair<int, int>, std::map<std::pair<int, int>, std::vector<glm::mat4>>> chunkMatrices;
     void updatePlayerPosition(const glm::vec3& newPosition);
     void checkAndLoadChunks();
-    const std::map<std::pair<int, int>, std::vector<glm::mat4>>& getChunkMatrices() const;
+    const std::map<std::pair<int, int>, std::map<std::pair<int, int>, std::vector<glm::mat4>>>& getChunkMatrices() const;
 
-
-
+    // Takes in the camera position and gets the height of the terrain at that point.
+    float getGroundHeight(glm::vec3 position);
 };
 
 #endif // TERRAINGENERATOR_H
