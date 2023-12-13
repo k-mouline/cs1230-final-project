e51aeb024fbc6aef4078ce9d112a25e60d048031
gdhanda26 <113314719+gdhanda26@users.noreply.github.com>
2023-12-12 21:34:10 -0500

    Update terraingenerator.h

--- src/terraingenerator.h
+++ src/terraingenerator.h
@@ -11,32 +11,30 @@ class TerrainGenerator
 {
 public:
     TerrainGenerator();
-    std::map<std::pair<int, int>, std::vector<Cube*>> createTranslationMatricesForChunk(int chunkX, int chunkY);
+    std::map<std::tuple<int, int, int>, Cube*> createTranslationMatricesForChunk(int chunkX, int chunkY);
     static float getOffset();
-    static const int chunkSize = 10;
+    static const int chunkSize = 8;
     static const int maxChunkHeight = 16;
     static const int chunkDepth  = 16;
     static const int offset = 1;
     float treeProbability = 0.01; // probability of generating a tree above grass (1% -> happens once in every 100 blocks generated)
     float treeHeight = -15; // generates trees above z_values > -15
 
-
     float getFractalNoise(FastNoiseLite noise, float x, float y, int octaves, float persistence);
 
-
     glm::vec3 playerPosition;
-    int renderDistance = 2; // Number of chunks to render in each direction from the player
+    int renderDistance = 3; // Number of chunks to render in each direction from the player
     std::vector<Cube*> updatePlayerPosition(const glm::vec3& newPosition, std::vector<Cube*> oldCubes, bool isFirst);
     bool checkAndLoadChunks();
-    const std::map<std::pair<int, int>, std::map<std::pair<int, int>, std::vector<Cube*>>>& getChunkMatrices() const;
+    const std::map<std::pair<int, int>, std::map<std::tuple<int, int, int>, Cube*>>& getChunkMatrices() const;
 
-    std::map<std::pair<int, int>, std::map<std::pair<int, int>, std::vector<Cube*>>> chunkMatrices1;
-    std::map<std::pair<int, int>, std::map<std::pair<int, int>, std::vector<Cube*>>> cachedChunkMatrices2;
+    std::map<std::pair<int, int>, std::map<std::tuple<int, int, int>, Cube*>> chunkMatrices1;
+    std::map<std::pair<int, int>, std::map<std::tuple<int, int, int>, Cube*>> cachedChunkMatrices2;
 
     std::vector<Cube*> cubesVector;
 
     // Takes in the camera position and gets the height of the terrain at that point.
-    float getGroundHeight(glm::vec3 position);
+    bool getGroundHeight(glm::vec3 position);
 };
 
 #endif // TERRAINGENERATOR_H
